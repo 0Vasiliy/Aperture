557862bea823a63c0033c35e7b1d18016a3658de
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-03-04 02:19:21 +0300

    Путь img

--- src/js/script.js
+++ src/js/script.js
@@ -31,28 +31,28 @@ let btnWild = document.querySelector('.btn_wildlife');
 
 btnPr.addEventListener('click',() => {  
     let link = document.createElement('a');
-    link.setAttribute('href', '../img/what/IMAGE_1.jpg');   
+    link.setAttribute('href', './img/what/IMAGE_1.jpg');   
 	link.setAttribute('download', 'product');   
 	link.click();
 	return false;
 })
 btnArh.addEventListener('click',() => {  
     let link = document.createElement('a');
-    link.setAttribute('href', '../img/what/IMAGE_2.jpg');   
+    link.setAttribute('href', './img/what/IMAGE_2.jpg');   
 	link.setAttribute('download', 'architecture');   
 	link.click();
 	return false;
 })
 btnDr.addEventListener('click',() => {  
     let link = document.createElement('a');
-    link.setAttribute('href', '../img/what/IMAGE_3.jpg');   
+    link.setAttribute('href', './img/what/IMAGE_3.jpg');   
 	link.setAttribute('download', 'drone');   
 	link.click();
 	return false;
 })
 btnWild.addEventListener('click',() => {  
     let link = document.createElement('a');
-    link.setAttribute('href', '../img/what/IMAGE_4.jpg');   
+    link.setAttribute('href', './img/what/IMAGE_4.jpg');   
 	link.setAttribute('download', 'wildlifet');   
 	link.click();
 	return false;
